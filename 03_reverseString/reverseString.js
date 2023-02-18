59b1a0e2b4e3235176d6af8539c46a99dfbad95c
Omar <124088826+oali220@users.noreply.github.com>
2023-02-18 14:35:06 -0600

    Program successfully reverses string. All test cases passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    var newStr = "";
+
+    for (var i = str.length - 1; i > -1; i--) {
+        newStr += str[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
