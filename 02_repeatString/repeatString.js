382530568b3f5f76e7d85fbb2a21f50171b894ba
Omar <124088826+oali220@users.noreply.github.com>
2023-02-18 14:16:07 -0600

    Function successfully repeats strings. All test cases passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    newStr = "";
+
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+
+        count = 0;
+        while(count < num) {
+            newStr += str;
+            count++;
+        }
+        return newStr;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
