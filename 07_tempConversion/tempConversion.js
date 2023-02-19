4d06db43e840731af8c0234d21718207f9b31c2f
Omar <124088826+oali220@users.noreply.github.com>
2023-02-18 22:42:02 -0600

    Converts between farh. and cels. Passed test cases.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  var cel = (far - 32) * (5 / 9);
+  var celRounded = Math.round(cel * 10) / 10;
+  return celRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  var far = (cel * (9 / 5)) + 32;
+  var farRounded = Math.round(far * 10) / 10;
+  return farRounded;
 };
 
 // Do not edit below this line
