39bb749d81c64415fcae79e0c1f0b417bab96bb8
Omar <124088826+oali220@users.noreply.github.com>
2023-02-18 22:13:29 -0600

    Successfully removes arguments from array. Passed all test cases.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
 
-};
+    array.forEach((ele) => {
+        if (!args.includes(ele)) {
+            newArray.push(ele);
+        }
+    });
+
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
