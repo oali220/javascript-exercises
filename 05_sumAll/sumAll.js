88c037cbddfde204ffabe007a6454771999d53dd
Omar <124088826+oali220@users.noreply.github.com>
2023-02-18 22:24:09 -0600

    Program successfully sums between (inclusive) 2 paramters passed in. Passed all test cases.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || Number.isInteger(num1) == false || Number.isInteger(num2) == false) {
+        return 'ERROR';
+    }
+    if (num1 > num2) {
+        max = num1;
+        min = num2;
+    }
+    else {
+        min = num1;
+        max = num2;
+    }
+    var sum = 0;
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
