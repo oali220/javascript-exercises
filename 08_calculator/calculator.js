ad26c14696f34d4f8854953f7661d0cb1767dc36
Omar <124088826+oali220@users.noreply.github.com>
2023-02-18 22:50:43 -0600

    Repoduced calculator operations. Passed test cases.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  var sum = 0;
+  array.forEach((ele) => {
+    sum += ele;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  var product = 1;
+  array.forEach((ele) => {
+    product *= ele;
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  }
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
